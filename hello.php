980197c4155784a26943ccb42c1bf2a3e46f6b1c
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-03-15 16:32:45 +0700

    first commit

---
+++ hello.php
@@ -0,0 +1,20 @@
+<?php
+    /*
+    echo nl2br ("Hello World\nHello World\nHello World\n");
+    
+    $t = date("H");
+
+    if ($t > "20"){
+        echo nl2br("It's Night\n{$t}");
+    }
+    else {
+        echo nl2br("It's Not Night Yet\n{$t}");
+    }
+    */
+    $t  = 5;
+
+    while ($t>=0){
+        echo("Number : {$t} <br>");
+        $t--;
+    }
+
---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contoh</title>
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="bootstrap/js/bootstrap.js" type="script">
+    </link>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="padding: 10px;">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03"
+                aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">Navbar</a>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <div class="jumbotron" style="background-color: burlywood; padding: 20px; margin-bottom: 10px;">
+        <h1 class="display-4">Steven Budi Sanjaya</h1>
+        <h2 class="display-3">K3519076</h2>
+        <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus illo aspernatur, voluptatum id
+            quasi debitis? A vel fugit iste voluptatum nam blanditiis, minima commodi ut est explicabo accusamus modi
+            quam.</p>
+        <hr class="my-4">
+        <p>Upload your file down here</p>
+        <p class="lead">
+            <input type="file" id="input_file">
+        </p>
+    </div>
+    
+    <div class="container" style="margin:20px; padding:20px;">
+        <!-- Example row of columns -->
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Heading</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+                    mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
+                    mollis euismod. Donec sed odio dui. </p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div>
+            <div class="col-md-4">
+                <h2>Heading</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+                    mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
+                    mollis euismod. Donec sed odio dui. </p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div>
+            <div class="col-md-4">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula
+                    porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
+                    ut fermentum massa justo sit amet risus.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div>
+        </div>
+
+        <hr>
+
+    </div>
+    <footer class="blog-footer">
+        <p style="text-align: center;">Pemrograman Web PTIK 2021</p>
+    </footer>
+</body>
+</html>
