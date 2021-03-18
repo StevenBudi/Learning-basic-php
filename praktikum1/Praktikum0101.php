7c5601faa8fe7c1c02971475bf857e72eb1b0122
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-03-18 21:27:02 +0700

    Organize file and renaming files

--- praktikum1/Praktikum0101.php
+++ praktikum1/Praktikum0101.php
@@ -1,15 +1,16 @@
 <?php
-    /*
     echo nl2br ("Hello World\nHello World\nHello World\n");
 
+
+    echo"<br/>";
     // Statement Control
     $t = date("H");
 
     if ($t > "20"){
-        echo nl2br("It's Night\n{$t}");
+        echo nl2br("It's Night {$t}\n");
     }
     else {
-        echo nl2br("It's Not Night Yet\n{$t}");
+        echo nl2br("It's Not Night Yet {$t}\n");
     }
     
     $t  = 5;
@@ -19,5 +20,4 @@
         echo("Number : {$t} <br>");
         $t--;
     }
-    */
     
--- praktikum1/index.php
+++ praktikum1/index.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contoh</title>
-    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
-    <link href="bootstrap/js/bootstrap.js" type="script">
+    <link href="../bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="../bootstrap/js/bootstrap.js" type="script">
     </link>
 </head>
 
