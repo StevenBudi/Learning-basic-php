272b02a52b2d908687bb9cebc56ecf6b4df7c228
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-04-10 15:17:40 +0700

    Pertanyaan bacafile2.php

--- praktikum4/bacafile2.php
+++ praktikum4/bacafile2.php
@@ -10,6 +10,15 @@
 
 
     /*
+    2.	Jalankan script di atas, dan amati hasilnya! Apakah output yang dihasilkan sama dengan script bacafile.php? 
+    Jika sama, apa kesimpulannya? Dan jelaskan apa perbedaan cara kerja dari kedua script tersebut!
+    Jawab : 
+    - Output yang dihasilkan oleh kedua file sama, kesimpulannya adalah kita bisa membaca file dalam PHP dengan menggunakan 2 cara yaitu membaca baris satu -persatu 
+        atau membaca karakternya satu - persatu.
+    - Script bacafile2.php membaca program dengan menggunakan pointer. Selama pointer masih menunjuk pada baris yang berisi data maka 
+        data tersebut akan di echo oleh program. Sedangkan script bacafile.php membaca data sesuai dengan jumlah byte data (karakter) 
+        yang ada pada file.
+    Dengan menggunakan fread() maka program akan membaca karakter pada file satu - persatu
     3.	Jelaskan kegunaan dari function berikut ini:
         a.	feof()
         Jawab: Mengecek pointer program pada file menunjukan EOF (end-of-file) / bagian akhir pada file
