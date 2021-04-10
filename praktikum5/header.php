e6f2b256593fb07ebdc117433e26f36042375a84
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-04-10 14:54:15 +0700

    necessary file for praktikum4 and praktikum5

---
+++ praktikum5/header.php
@@ -0,0 +1,25 @@
+<?php
+    function headerDecor(){
+        echo(
+            '<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Learning Basic PHP</title>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" type="text/css">
+            </head>
+            <body>
+                <header class="container-fluid" style="background-color: black;height:100px;display:flex;align-items:center;flex-direction:column;justify-content:center;margin-bottom:25px;">
+                    <h1 style="color:green;">Learning Basic PHP</h1>
+                </header>
+            </body>
+            </html>'
+        
+        
+        
+        );
+    }
+
+?>
