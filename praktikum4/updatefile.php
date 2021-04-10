cb82d323b5b60f452190f928482091e87588df29
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-04-10 15:24:32 +0700

    uptadefile

---
+++ praktikum4/updatefile.php
@@ -0,0 +1,20 @@
+<?php
+    include './header.php';
+    headerDecor();
+    $namaFile = "myfile.txt";
+    $myfile = fopen($namaFile, "a") or die("Tidak bisa buka file!");
+    fwrite($myfile, "DOS = Disk Operating System\n");
+    while (!feof($myfile)) {
+        echo fgets($myfile);
+    }
+    fclose($myfile);
+
+
+    /*
+    3.	Apa kegunaan dari function fwrite()?
+    Jawab : Menuliskan sesuatu pada file
+    4.	Apa yang terjadi jika perintah fopen($namaFile, "a") diganti dengan fopen($namaFile, "w")?
+    Jawab : Maka isi file sebelumnya akan ter-overwrite jika kita menjalankan function fwrite()
+
+
+    */
