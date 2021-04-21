7b30cceb61e1a1dc12e45e6940d630fc6448fd4e
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-04-21 22:52:05 +0700

    fixing bugs

--- praktikum6/Latihan/game.php
+++ praktikum6/Latihan/game.php
@@ -1,51 +1,50 @@
 <?php
+    error_reporting(E_ALL ^ E_NOTICE);
     include("./header.php");
     headerDecor();
     date_default_timezone_set("Asia/Jakarta");
-    $bil = rand(0, 101);
+    // $bil = rand(0, 100);
     $score = 0;
-
-
+    $bil = $_COOKIE['random'];
+    echo"
+        <script>console.log('$bil');</script>
+    ";
+    echo('
+    <p>
+        <strong>
+            Hallo, nama saya Mr. PHP. Saya telah memilih secara random sebuah bilangan bulat. Silakan Anda menebak ya!
+            Hallo, nama saya Mr. PHP. Saya telah memilih secara random sebuah bilangan bulat. Silakan Anda menebak ya!
+        </strong>
+    </p>
+    <form  method="get">
+        Tebakan Anda <input type="text" name="tebak"> <input type="submit" value="Submit">
+    </form>    
+    ');
+    $guess = $_GET['tebak'];
     
-
-    while(true){
-        echo('
-        <form  method="get">
-            Tebakan Anda <input type="text" name="tebak"> <input type="submit" value="Submit">
-        </form>    
-        ');
-        $guess = $_GET['tebak'];
+    if (isset($guess)){
         $score+=1;
         if ($guess > $bil) {
-            echo("<pre>
-            Hallo, nama saya Mr. PHP. Saya telah memilih secara random sebuah bilangan bulat. Silakan Anda menebak ya!
-            
+            echo("
             Waaah… masih salah ya, bilangan tebakan Anda terlalu tinggi.
-             </pre>");
+            ");
         }else if($guess < $bil){
-           echo("
-           <pre>
-           Hallo, nama saya Mr. PHP. Saya telah memilih secara random sebuah bilangan bulat. Silakan Anda menebak ya!
-           
-           Waaah… masih salah ya, bilangan tebakan Anda terlalu rendah.
-            </pre>");
-
+            echo("
+            Waaah… masih salah ya, bilangan tebakan Anda terlalu rendah.
+                ");
         }else{
-            break;
-            
+            // setcookie('high-score', $score, time()+24*3600);
+            setcookie('random', "", time()-24*3600);
+            setcookie('random', rand(0, 100), time()+24*3600);
+            echo"
+            Selamat ya… Anda benar, saya telah memilih bilangan $bil.
+            ";
+        
+            echo'
+            <a href="game.php">Restart</a>
+            ';
         }
     }
-    if($_COOKIE['high-score'] > $score && $_COOKIE['high-score'] != 0){
-        setcookie('high-score', $score, time()+24*3600);
-    }
 
-    echo"<pre>
-    Selamat ya… Anda benar, saya telah memilih bilangan $bil.
-    </pre>";
+    
 
-    echo'
-    <a href="game.php">Restart</a>
-    ';
-    
-    
-?>
--- praktikum6/Latihan/login.php
+++ praktikum6/Latihan/login.php
@@ -2,12 +2,16 @@
     include ("./header.php");
     headerDecor();
     $expire = time()+24*3600;
+
     if(isset($_POST['submit'])){
-        setcookie("player", $_POST["username"], $expire);
-        setcookie("high-score", 0, $expire);
-        if(isset($_COOKIE['player'])){
-            header("Location : game.php");
-        }
+        setcookie("username", $_POST["username"], $expire);
+        // setcookie("high-score", 0, $expire);
+        setcookie("random", rand(0, 100), $expire);
+
+        
+    }
+    if(isset($_COOKIE['username'])){
+        header("Location: game.php");
     }
     else{
 ?>    
@@ -16,7 +20,7 @@
     <p>Silahkan masukkan nama anda terlebih dahulu</p>
     <form action="login.php" method="POST">
         Nama Anda <input type="text" name="username">
-        <input type="submit" value="Submit">
+        <input type="submit" value="Submit" name="submit">
     </form>
 <?php
     }
