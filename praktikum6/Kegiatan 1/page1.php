1c60c5b476d051080b2b70eb6c61199d4bdcaf85
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-04-21 12:14:22 +0700

    fixing bugs

--- praktikum6/Kegiatan 1/page1.php
+++ praktikum6/Kegiatan 1/page1.php
@@ -1,11 +1,19 @@
 <?php
     session_start();
 
-    echo "<h1>Page 1</h1>";
-    // menampilkan nama lengkap usernya
-    echo "<p>Selamat datang ".$_SESSION['namauser']."</p>";
+    if(isset($_SESSION['namauser'])){
+        echo "<h1>Page 1</h1>";
+        // menampilkan nama lengkap usernya
+        echo "<p>Selamat datang ".$_SESSION['namauser']."</p>";
 
-    echo "<h2>Menu Utama</h2>";
-    echo "<p><a href='page1.php'>Page 1</a> | <a href='page2.php'>Page 2</a> | <a href='page3.php'>Page 3</a> | <a href='logout.php'>Logout</a></p>";
+        echo "<h2>Menu Utama</h2>";
+        echo "<p><a href='page1.php'>Page 1</a> | <a href='page2.php'>Page 2</a> | <a href='page3.php'>Page 3</a> | <a href='logout.php'>Logout</a></p>";
+    }
+    else{
+        echo '<script>alert("Silahkan Login Terlebih Dahulu");
+            window.location.href=(\'form.html\')
+        </script>';
+        
+    }
 
 ?>
