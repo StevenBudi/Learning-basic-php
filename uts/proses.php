5ad8ce2e249f11a429c15fcb4cece46fcdea669f
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-04-29 09:29:40 +0700

    design game func

--- uts/proses.php
+++ uts/proses.php
@@ -1,7 +1,13 @@
 <?php
+    session_start();
     if(isset($_POST['submit'])){
-        setcookie('login', true, 3600*24*365.25*1000);
-        setcookie('player', $_POST['username'],  3600*24*365.25*1000);
+        setcookie('login', true, time() + 3600*24*365.25*1000);
+        setcookie('player', $_POST['username'], time() + 3600*24*365.25*1000);
+        $_SESSION['bil1'] = rand(0, 20);
+        $_SESSION['bil2'] = rand(0, 20);
+        $_SESSION['nyawa'] = 0;
+        $_SESSION['score'] = 0;
+        header('Location:dashboard.php');
     }else{
         echo ('<script>alert("Silahkan Login Terlebih Dahulu");
             window.location.href=(\'login.php\')
