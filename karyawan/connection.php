48d587794c0e68fc109d13b7078b5cce5139b6d3
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-05-03 20:34:53 +0700

    adding pegawai.php

--- karyawan/connection.php
+++ karyawan/connection.php
@@ -1,4 +1,33 @@
 <?php
+    function fetchData($dbhost, $dbuser, $dbpass, $dbname, $port, $dbtable){
+        $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname, $port);
 
+        if (!$conn){
+?>
+        <script>console.log('Connection Error');</script>
+<?php
+        }else{
+?>
+        <script>console.log('Connection Success')</script>
+<?php
+        }
+        $query = "SELECT * FROM $dbtable ORDER BY `id_karyawan` DESC";
+        $search = mysqli_query($conn, $query);
+        if (mysqli_num_rows($search) > 0){
+?>
+            <table>
+                <tr>
+                    <th>No</th>
+                    <th>Nama</th>
+                    <th>Email</th>
+                    <th>Jenis Kelamin</th>
+                </tr>
+            </table>
+<?php
+            while($row = mysqli_fetch_assoc($search)){
+                ;
+            }
+        }
+    }
 
 ?>
