0dcccbf9328658819631b317ea72b372ab4bb367
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-05-03 20:45:00 +0700

    data fetching function

--- karyawan/connection.php
+++ karyawan/connection.php
@@ -15,18 +15,28 @@
         $search = mysqli_query($conn, $query);
         if (mysqli_num_rows($search) > 0){
 ?>
-            <table>
-                <tr>
-                    <th>No</th>
-                    <th>Nama</th>
-                    <th>Email</th>
-                    <th>Jenis Kelamin</th>
-                </tr>
-            </table>
+            <div>
+                <table>
+                    <tr>
+                        <th>Id Pegawai</th>
+                        <th>Nama</th>
+                        <th>Email</th>
+                        <th>Jenis Kelamin</th>  
+                    </tr>
 <?php
             while($row = mysqli_fetch_assoc($search)){
-                ;
+                echo("<tr>
+                        <td>{$row['id_karyawan']}</td>
+                        <td><a href='pegawai.php?id={$row['id_karyawan']}'>{$row['nama']}</a></td>
+                        <td>{$row['email']}</td>
+                        <td>{$row['jenis_kelamin']}</td>
+                    </tr>");
             }
+?>
+            
+                </table>
+            </div>
+<?php        
         }
     }
 
