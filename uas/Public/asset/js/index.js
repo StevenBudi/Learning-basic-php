4a328c0af9fb1c72df810c207678a1f3608c106e
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-05-05 08:07:56 +0700

    mapping

---
+++ uas/Public/asset/js/index.js
@@ -0,0 +1,35 @@
+var _this = this;
+var includeHTML = function (className) {
+    var elements = document.getElementsByTagName("*");
+    var _loop_1 = function (index) {
+        var element = elements[index];
+        var file = element.getAttribute(className);
+        if (file) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (_this.readyState === 4) {
+                    if (_this.status === "200") {
+                        element.innerHTML = _this.responseText;
+                    }
+                    if (_this.status === "400") {
+                        element.innerHTML = "Element Not Found";
+                    }
+                    else {
+                        console.log(_this.status);
+                    }
+                    element.removeAttribute(className);
+                    includeHTML(className);
+                }
+            };
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return { value: void 0 };
+        }
+    };
+    for (var index = 0; index < elements.length; index++) {
+        var state_1 = _loop_1(index);
+        if (typeof state_1 === "object")
+            return state_1.value;
+    }
+};
+
