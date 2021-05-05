3238c45986618a0ed3ee05ba386281d093983795
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-05-05 17:54:20 +0700

    fixing style

--- uas/index.php
+++ uas/index.php
@@ -6,15 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="stylesheet" href="./asset/style/style.css">
+    <link rel="stylesheet" href="./asset/style/style.css" type="text/css">
 </head>
 <body>
-    <?php include('./asset/template/navbar.php') ?>
+    <!-- <?php include('./asset/template/navbar.php') ?> -->
     <div class="container container-fluid">
+        
         <h1>Hello World</h1>
     </div>
     
-    <?php include('./asset/template/footer.php')?>
+    <!-- <?php include('./asset/template/footer.php')?> -->
     <script src="./asset/js/index.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 </body>
