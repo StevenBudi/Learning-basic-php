30a59232a533b6b37ddaaf4592c5aba939059f99
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-05-06 21:26:52 +0700

    adding database config

---
+++ uas/Modules/dbconfig.php
@@ -0,0 +1,10 @@
+<?php
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $dbname = "";
+    $port   = 3310;
+    $table  = "";
+
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname, $port, $table)
+?>
