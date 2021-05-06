c5b658ef867e09697db0eb44a5c2047274bb1841
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-05-06 12:20:37 +0700

    navbar

--- uas/asset/js/index.js
+++ uas/asset/js/index.js
@@ -1,5 +1,15 @@
 const hello = () => {
     alert("Hello");
 };
-hello();
+const activeLink = () => {
+    const elements = document.getElementsByClassName("nav-link");
+    for (let index = 0; index < elements.length; index++) {
+        elements[index].addEventListener("click", () => {
+            const current = document.getElementsByClassName(" active");
+            current[0].className = current[0].className.replace(" active", "");
+            elements[index].className += " active";
+        });
+    }
+};
+activeLink();
 //# sourceMappingURL=index.js.map
--- uas/asset/template/navbar.php
+++ uas/asset/template/navbar.php
@@ -1,3 +1,30 @@
-<?php echo(' <nav>
-    <h1>This is Navbar</h1>
-</nav>') ?>
+<?php echo(' <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-2">
+	<div class="container-fluid navigation">
+		<a class="navbar-brand" href="#"><h1>Navbar</h1></a>
+		<button
+			class="navbar-toggler"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarTogglerDemo02"
+			aria-controls="navbarTogglerDemo02"
+			aria-expanded="false"
+			aria-label="Toggle navigation"
+		>
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+			<ul class="navbar-nav me-auto">
+				<li class="nav-item">
+					<a class="nav-link active" href="#" id="nav-link"><h3>Item1</h3></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#" id="nav-link"><h3>Item2</h3></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#" id="nav-link"><h3>Item3</h3></a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+') ?>
