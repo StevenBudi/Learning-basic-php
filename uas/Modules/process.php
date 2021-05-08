6f6b1429a381a03272446b765770659b35642e49
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-05-08 16:18:26 +0700

    form process

---
+++ uas/Modules/process.php
@@ -0,0 +1,18 @@
+<?php
+/*
+    TODO
+
+    Part 1 : 
+    process user form
+        - check if user already reserved or not
+        - check date and reservation time
+
+    redirect/sent user email for confirmation/cancellation (using crsf token) if success
+    sent back user to reserve page if fail but kept user input intact with input field
+
+    Part 2 :
+    assign reservation to table based on people
+    alter table availability and input reservation information if success
+    
+
+*/
