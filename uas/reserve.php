869cb616e351c20ac2365a0384df2faf9306adbd
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-05-12 15:36:08 +0700

    fixing option

--- uas/reserve.php
+++ uas/reserve.php
@@ -45,8 +45,8 @@
 					<select name="people" id="peole" class="form-select" required>
 						<option value="">Choose People Number</option>
 						<option value="2">2</option>
-						<option value="4">4</option>
-						<option value="6">6</option>
+						<option value="4">3 - 4</option>
+						<option value="6">5 - 6</option>
 						<option value="10">7 - 10</option>
 					</select>
 				</td>
