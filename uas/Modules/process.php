1034a38356e3ae74cd077458f9fadd5008a5d4b6
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-05-12 08:19:43 +0700

    change token

--- uas/Modules/process.php
+++ uas/Modules/process.php
@@ -24,7 +24,7 @@
         $reserveData -> people  = $_POST['people'];
         $reserveData -> datetime = date_format(date_create($_POST['reser_date']." ".$_POST['reser_time'].":00:00"), "Y-m-d H:i:s");
         $reserveData -> notes   = $_POST['reser_notes'];
-        $reserveData -> token = bin2hex($_POST['email']);
+        $reserveData -> token = bin2hex($_POST['first_name']." {$_POST['last_name']}");
         
         $reserJson = json_encode($reserveData);
         echo $reserJson;
