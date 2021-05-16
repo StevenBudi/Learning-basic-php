9f6ffee2bb352ab13364fe0055b548344fc7d7b7
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-05-16 09:40:26 +0700

    update purge.py

--- uas/asset/purge.py
+++ uas/asset/purge.py
@@ -1,8 +1,15 @@
 import mysql.connector as conn
 from mysql.connector import Error
 
+host_args = {
+    "host":"localhost",
+    "database":"restaurant",
+    "user":"root",
+    "password":''
+}
+
 try:
-    koneksi = conn.connect(host='localhost', database='restaurant', user='root', password='')
+    koneksi = conn.connect(**host_args)
     if koneksi.is_connected():
         DB_INFO = koneksi.get_server_info()
         print(f"Connected to MySQL Server version - {DB_INFO}")
@@ -11,22 +18,22 @@ try:
         cursor.execute(query)
         record = cursor.fetchone()
         print("You're connected to database: ", record)
-        query1 = "DELETE FROM `reservation_customer`;"
-        query2 = "UPDATE `restaurant_table` SET availability='true' WHERE availability='false';"
-        query3 = "DELETE FROM `reservation_detail`;"
-        cursor.execute(query1)
-        koneksi.commit()
-        print("Success delete all data in `reservation_customer`")
-        cursor.execute(query2)
-        koneksi.commit()
-        print("Success set all data in `restaurant_table`")
-        cursor.execute(query3)
-        koneksi.commit()
-        print("Success delete all data in `reservation_detail`")
+        SQL = '''DELETE FROM `reservation_customer`;
+            UPDATE `restaurant_table` SET availability='true' WHERE availability='false';
+            DELETE FROM `reservation_detail`;
+            ALTER TABLE `reservation_detail` AUTO_INCREMENT=0;
+            '''
+        # cursor.execute("DELETE FROM `reservation_customer`;")
+        # cursor.execute("UPDATE `restaurant_table` SET availability='true' WHERE availability='false';")
+        # cursor.execute("DELETE FROM `reservation_detail`;")
+        # cursor.execute("ALTER TABLE `reservation_detail` AUTO_INCREMENT=0;")
+        for result in cursor.execute(SQL, multi=True):
+            print(result)
 except Error as e:
     print(e)
 finally:
     if koneksi.is_connected():
+        koneksi.commit()
         cursor.close()
         koneksi.close()
         print("Connection closed")
