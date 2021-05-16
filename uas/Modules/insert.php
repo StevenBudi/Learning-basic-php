e4662b341e591ddceac4e7b24763048eb38db26f
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-05-16 10:28:37 +0700

    mutate function for reuse

--- uas/Modules/insert.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include_once("./dbconfig.php");
-    $query = "INSERT INTO";
-    $search = mysqli_query($conn, $query);
-
-    
-    mysqli_close($conn);
-?>
--- uas/Modules/reservation.php
+++ uas/Modules/reservation.php
@@ -25,54 +25,8 @@
     
 </body>
 <?php
-    function fetchReserDetails($connection, $table, $id){
-        $reservation_result = mysqli_query($connection, "SELECT * FROM $table WHERE reservation_id='$id'");
-        if(!$reservation_result){
-            die("Something went wrong   : ".mysqli_error($connection));
-        }else{
-            return mysqli_fetch_assoc($reservation_result);
-        }
-    }
-
-    function fetchCustomerInfo($connection, $table, $data){
-        $customer_result = mysqli_query($connection, "SELECT * FROM $table WHERE customer_name = '{$data['customer_name']}'");
-        if(!$customer_result){
-            die("Something went wrong   : ".mysqli_error($connection));
-        }else{
-            return mysqli_fetch_assoc($customer_result);
-        }
-    }
-
-    function updateCustomerInfo($connection, $table, $data1, $data2){
-        $updateQuery = "UPDATE $table SET reservation_note = '{$_POST['notes']}', customer_phone= '{$_POST['phone']}' WHERE customer_name = '{$data1['customer_name']}'";
-        $res3 = mysqli_query($connection, $updateQuery);
-        if(!$res3){
-            die("Something went wrong : ".mysqli_error($connection));
-        }else{
-            ?>
-                <script>
-                    Swal.fire({
-                        icon:"success",
-                        title: "Success!",
-                        text:"Your data has been succesfully updated"
-                    })
-                </script>
-            <?php
-            $temp = fetchCustomerInfo($connection, $table, $data2);
-            return $temp;
-        }
-    }
-
-    function getOAuth($connection, $table1, $table3, $id){
-        $authQuery = mysqli_query($connection, "SELECT $table1.customer_token FROM $table1, $table3 WHERE $table3.reservation_id = $id AND $table1.customer_name = $table3.customer_name");
-        if(!$authQuery){
-            return false;
-        }else{
-            return mysqli_fetch_assoc($authQuery)['customer_token'];
-        }
-    }
-
     error_reporting(E_ERROR | E_PARSE);
+    include('./functionality.php');
     include('./dbconfig.php');
     $table1 = $table_customer;
     $table2 = $table_info;
