38461ea64469ad829c070d643bdad0be02ee05ff
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-05-16 10:00:50 +0700

    update reservation date to tommorow

--- uas/asset/js/form.js
+++ uas/asset/js/form.js
@@ -51,21 +51,23 @@ const expandCol = () => {
     }
 };
 const limitDate = () => {
+    const today = new Date();
     const date = new Date();
+    date.setDate(today.getDate() + 1); // Set date to tomorrow
     const minDate = `${date.getFullYear()}-${date.getMonth() < 10 ? "0" + (date.getMonth() + 1).toString() : (date.getMonth() + 1)}-${date.getDate() < 10 ? "0" + date.getDate().toString() : date.getDate()}`;
-    const maxDate = () => {
-        const result = new Date();
-        result.setDate(date.getDate() + 0); // set how many days from today for max value
-        return `${result.getFullYear()}-${result.getMonth() < 10 ? "0" + (result.getMonth() + 1).toString() : (result.getMonth() + 1)}-${result.getDate() < 10 ? "0" + result.getDate().toString() : result.getDate()}`;
-    };
+    // const maxDate = () => {
+    //     const result = new Date()
+    //     result.setDate(date.getDate() + 0) // set how many days from today for max value
+    //     return `${result.getFullYear()}-${result.getMonth() < 10 ? "0" + (result.getMonth() + 1).toString() : (result.getMonth() + 1)}-${result.getDate() < 10 ? "0" + result.getDate().toString() : result.getDate()}`
+    // }
     try {
         // Set Min value
         document.getElementById("reser_date").setAttribute("min", minDate);
         // Set Max value
-        document.getElementById("reser_date").setAttribute("max", maxDate());
+        document.getElementById("reser_date").setAttribute("max", minDate);
         // Set value
         const element = document.getElementById("reser_date");
-        element.value = maxDate();
+        element.value = minDate;
     }
     catch (error) {
         console.error();
