ad63e19fa8d11b2475e6ee26243eda0a3ad63c0b
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-05-18 11:19:30 +0700

    update cron

--- uas/cron/refresh.php
+++ uas/cron/refresh.php
@@ -2,7 +2,7 @@
     include('../Modules/dbconfig.php');
     $table = $table_info;
     $table2 = $reservation_detail;
-    $result = mysqli_multi_query($conn, "UPDATE $table SET availability='true' WHERE availability='false';DELETE FROM $table2 WHERE status='check-out' OR status='cancelled';");
+    $result = mysqli_multi_query($conn, "DELETE FROM $table2 WHERE status='check-out' OR status='cancelled';");
     if(!$result){
         die("Something went wrong : ".mysqli_error($conn));
     }
