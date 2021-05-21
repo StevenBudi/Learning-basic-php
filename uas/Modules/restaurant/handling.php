e5e792e961044be16710a3b217d1f767b1add6f9
Steven Budi <57691998+StevenBudi@users.noreply.github.com>
2021-05-21 13:04:51 +0700

    adding handler

---
+++ uas/Modules/restaurant/handling.php
@@ -0,0 +1,13 @@
+<?php
+    include('../dbconfig.php');
+    $request = $_SERVER['REQUEST_METHOD'];
+    switch ($request){
+        case 'POST':
+            break;
+        default:
+            // Alert Then Redirect
+            break;
+    }
+
+
+?>
