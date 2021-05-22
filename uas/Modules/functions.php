e8b2c149cbea1464b26e6731d02b6733a0cbfea3
StevenBudi <57691998+StevenBudi@users.noreply.github.com>
2021-05-22 17:45:37 +0700

    change file name

---
+++ uas/Modules/functions.php
@@ -0,0 +1,77 @@
+<?php
+    // Get Reservation Details
+    function fetchReserDetails($connection, $table, $id){
+            $reservation_result = mysqli_query($connection, "SELECT * FROM $table WHERE reservation_id='$id'");
+            if(!$reservation_result){
+                die("Something went wrong   : ".mysqli_error($connection));
+            }else{
+                return mysqli_fetch_assoc($reservation_result);
+            }
+    }
+
+    // Get Customer Info Using Name
+    function fetchCustomerInfo($connection, $table, $name){
+            $customer_result = mysqli_query($connection, "SELECT * FROM $table WHERE customer_name = '$name'");
+            if(!$customer_result){
+                die("Something went wrong   : ".mysqli_error($connection));
+            }else{
+                return mysqli_fetch_assoc($customer_result);
+            }
+    }
+
+    // Update Customer Information
+    function updateCustomerInfo($connection, $table, $data, $name){
+            $info_result = mysqli_query($connection, "UPDATE $table SET reservation_note = '{$_POST['notes']}', customer_phone= '{$_POST['phone']}' WHERE customer_name = '$name");
+            if(!$info_result){
+                die("Something went wrong : ".mysqli_error($connection));
+            }else{
+                ?>
+                    <script>
+                        Swal.fire({
+                            icon:"success",
+                            title: "Success!",
+                            text:"Your reservation has been confirmed !"
+                        }).then(() => {
+                            window.location.href="../../index.php";
+                        });
+                    </script>
+                <?php
+                $data = fetchCustomerInfo($connection, $table, $name);
+                return $data;
+            }
+    }
+
+    // Get Authentication ID
+    function getOAuth($connection, $table1, $table2, $id){
+            $authResult = mysqli_query($connection, "SELECT customer_token, status FROM $table1, $table2 WHERE $table2.reservation_id = $id AND $table1.customer_name = $table2.customer_name");
+            if(!$authResult){
+                return false;
+            }else{
+                return mysqli_fetch_assoc($authResult);
+            }
+    }
+
+    // Update Reservation Detail's Status
+    function updateReserDetails($connection, $table, $id, $state){
+        $updateResult = mysqli_query($connection, "UPDATE $table SET status='$state' WHERE reservation_id= '$id'");
+        if(!$updateResult){
+            die("Something went wrong   : ".mysqli_error($connection));
+        }
+        
+    }
+
+    // Delete Customer
+    function deleteCustomer($connection, $table, $name){
+        $deleteResult = mysqli_query($connection, "DELETE FROM $table WHERE customer_name='$name'");
+        if(!$deleteResult){
+            die("Something went wrong : ".mysqli_error($connection));
+        }
+    }
+
+    // Update Table Status
+    function updateTable($connection, $table, $id){
+        $tableResult = mysqli_query($connection, "UPDATE $table SET availability='true' WHERE id = '$id'");
+        if(!$tableResult){
+            die("Something went wrong   : ".mysqli_error($connection));
+        }
+    }
