d177399e259430de02cd02dd0dea776a7b789eb7
StevenBudi <57691998+StevenBudi@users.noreply.github.com>
2021-05-23 17:50:35 +0700

    data handling

--- uas/Modules/restaurant/handling.php
+++ uas/Modules/restaurant/handling.php
@@ -5,7 +5,9 @@
     
     switch ($request){
         case 'PUT':
-            updateReserDetails($conn, $table_info, $_GET['id'], $_GET['state']);
+            $data = json_decode(file_get_contents("php://input"), true);
+            var_dump($data);
+            updateReserDetails($conn, $table_info, $data['ID'], $data['State']);
             break;
         default:
             // Alert Then Redirect
