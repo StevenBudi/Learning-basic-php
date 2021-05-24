c6c7e7ec53a3aba9045a66a6e886fdf38d671620
StevenBudi <57691998+StevenBudi@users.noreply.github.com>
2021-05-24 20:49:37 +0700

    article form

--- FTP/index.php
+++ FTP/index.php
@@ -9,6 +9,33 @@
 
 <body>
     <?php include('./navbar.php'); ?>
+
+    <main>
+        <div class="container container-fluid mt-5">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="card-title">Tulis Artikel Anda</h1>
+                    <hr>
+                    <form action="simpan.php" method="post" class="mt-2">
+                        <div class="mb-3">
+                            <label for="judul" class="form-label">Judul Artikel Anda</label>
+                            <input type="text" name="judul" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="author" class="form-label">Nama Anda</label>
+                            <input type="text" name="author" class="form-control">
+                        </div>
+                        <div class="mb-3">
+                            <label for="judul" class="form-label">Artikel Anda</label>
+                            <textarea name="artikel" cols="30" rows="5" class="form-control"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </main>
 </body>
 
 </html>
