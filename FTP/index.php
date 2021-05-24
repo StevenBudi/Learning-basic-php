828f1b26b8942e11754b323316d8b4347ac19a66
StevenBudi <57691998+StevenBudi@users.noreply.github.com>
2021-05-24 20:22:37 +0700

    templating

--- FTP/index.php
+++ FTP/index.php
@@ -1,2 +1,14 @@
-<?php
 // Home page
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('./header.php'); ?>
+    <title>FTP - Home</title>
+</head>
+
+<body>
+    <?php include('./navbar.php'); ?>
+</body>
+
+</html>
