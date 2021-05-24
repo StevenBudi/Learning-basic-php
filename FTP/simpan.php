34dfd0f15484503504c4f72fdbd4d11ebaca296f
StevenBudi <57691998+StevenBudi@users.noreply.github.com>
2021-05-24 21:38:40 +0700

    fixing saves

--- FTP/simpan.php
+++ FTP/simpan.php
@@ -11,15 +11,14 @@
 // Simpan File
     $judul = trim($_POST['judul']);
     
-    if(preg_match("[\W]{1,}", $judul)){
+    if(preg_match("([\W]{1,})", $judul)){
         ?>
         <script>ErrorAlert();</script>
         <?php
     }else{
         $author = trim($_POST['author']);
         $article = trim($_POST['artikel']);
-        $namaFile = "files\{$judul}.txt";
-        $file = fopen($namaFile, "w");
+        $file = fopen("$judul.txt", "w");
         fwrite($file, strtoupper($judul));
         fwrite($file, "\n\nAuthor : {$author}\n\n");
         fwrite($file, $article);
