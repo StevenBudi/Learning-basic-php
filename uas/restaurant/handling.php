2184764ff6bff01659e6f7e360ba2a5ba0381ba7
StevenBudi <57691998+StevenBudi@users.noreply.github.com>
2021-05-25 11:27:04 +0700

    adding PUT handler

--- uas/restaurant/handling.php
+++ uas/restaurant/handling.php
@@ -5,8 +5,14 @@
     switch ($request){
         case 'PUT':
             $data = json_decode(file_get_contents("php://input"), true);
-            var_dump($data);
+            $reser_details = fetchReserDetails($conn, $table_info, $data['ID']);
             updateReserDetails($conn, $reservation_detail, $data['ID'], $data['State']);
+            if($data['State'] === 'cancelled' || $data['State'] === 'check-out'){
+                updateTable($conn, $table_info, $reser_details['table_id']);                    
+            }
+            break;
+        case 'DELETE':
+            // Delete All Reservation Detail and Customer Today
             break;
         default:
             // Alert Then Redirect
