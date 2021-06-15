d360c981223b943c43f00e1f343334bb674a4e22
StevenBudi <57691998+StevenBudi@users.noreply.github.com>
2021-06-15 21:13:29 +0700

    request handling

--- AJAX/server.php
+++ AJAX/server.php
@@ -1,14 +1,42 @@
 <?php
+    $res = array();
     include("koneksi.php");
     $request = $_SERVER['REQUEST_METHOD'];
+    $index = 0;
     switch ($request) {
         case 'CITY':
-            # code...
+            $query = "SELECT * FROM regencies WHERE province_id = {$_GET['city']}";
+            $res = mysqli_query($conn, $query);
+            
+            while($row = mysqli_fetch_assoc($res)){
+                $res[$index][$id] = $row["id"];
+                $res[$index][$name] = $row["name"];
+                $index++;
+            }
+            return json_decode($res, true);
             break;
         case 'DISTRICT':
+            $query = "SELECT * FROM districts WHERE regency_id = {$_GET['district']}";
+            $res = mysqli_query($conn, $query);
+            
+            while($row = mysqli_fetch_assoc($res)){
+                $res[$index][$id] = $row["id"];
+                $res[$index][$name] = $row["name"];
+                $index++;
+            }
+            return json_decode($res, true);
             break;
 
         case 'RESIDENT':
+            $query = "SELECT * FROM villages WHERE district_id = {$_GET['resident']}";
+            $res = mysqli_query($conn, $query);
+            
+            while($row = mysqli_fetch_assoc($res)){
+                $res[$index][$id] = $row["id"];
+                $res[$index][$name] = $row["name"];
+                $index++;
+            }
+            return json_decode($res, true);
             break;
         default:
             # code...
