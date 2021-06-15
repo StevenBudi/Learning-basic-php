2499e2d75fbe77289eeda74a673dc090e7156928
StevenBudi <57691998+StevenBudi@users.noreply.github.com>
2021-06-15 21:25:21 +0700

    change request handling

--- AJAX/server.php
+++ AJAX/server.php
@@ -4,31 +4,10 @@
     $request = $_SERVER['REQUEST_METHOD'];
     $index = 0;
     switch ($request) {
-        case 'CITY':
-            $query = "SELECT * FROM regencies WHERE province_id = {$_GET['city']}";
-            $res = mysqli_query($conn, $query);
-            
-            while($row = mysqli_fetch_assoc($res)){
-                $res[$index][$id] = $row["id"];
-                $res[$index][$name] = $row["name"];
-                $index++;
-            }
-            return json_decode($res, true);
-            break;
-        case 'DISTRICT':
-            $query = "SELECT * FROM districts WHERE regency_id = {$_GET['district']}";
-            $res = mysqli_query($conn, $query);
-            
-            while($row = mysqli_fetch_assoc($res)){
-                $res[$index][$id] = $row["id"];
-                $res[$index][$name] = $row["name"];
-                $index++;
-            }
-            return json_decode($res, true);
-            break;
-
-        case 'RESIDENT':
-            $query = "SELECT * FROM villages WHERE district_id = {$_GET['resident']}";
+        case 'GET':
+            $query = isset($_GET['city']) ? "SELECT * FROM regencies WHERE province_id = {$_GET['city']}" : 
+            (isset($_GET['district']) ? "SELECT * FROM districts WHERE regency_id = {$_GET['district']}" : 
+            "SELECT * FROM villages WHERE district_id = {$_GET['resident']}");
             $res = mysqli_query($conn, $query);
             
             while($row = mysqli_fetch_assoc($res)){
