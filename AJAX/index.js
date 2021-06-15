bce468c82ecc1db74d7991653f5a33ca00b7ab2e
StevenBudi <57691998+StevenBudi@users.noreply.github.com>
2021-06-15 21:14:35 +0700

    adding more ajax

--- AJAX/index.js
+++ AJAX/index.js
@@ -1,32 +1,51 @@
-"use strict";
-const xhttp = new XMLHttpRequest();
-let params;
-const getOptSelectedID = (inputID, datalistID) => {
-    const input = document.getElementById(inputID);
-    const elements = document.getElementById(datalistID);
-    for (let index = 0; index < elements.options.length; index++) {
+var xhttp = new XMLHttpRequest();
+var params;
+var getOptSelectedID = function (inputID, datalistID) {
+    var input = document.getElementById(inputID);
+    var elements = document.getElementById(datalistID);
+    for (var index = 0; index < elements.options.length; index++) {
         if (elements.options[index].value === input.value) {
             return elements.options[index].getAttribute("id");
         }
     }
     return "";
 };
-const getCity = () => {
+var getCity = function () {
     params = getOptSelectedID("province", "provinceData");
-    xhttp.onreadystatechange = () => {
+    xhttp.onreadystatechange = function () {
         if (xhttp.readyState === xhttp.DONE) {
             if (xhttp.response === "OK")
                 // Append result to datalist/select
                 console.log(xhttp.response);
         }
     };
-    xhttp.open("CITY", "./server.php");
+    xhttp.open("CITY", "server.php?city=" + params);
     xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     xhttp.send();
 };
-const getDistrict = () => {
-    console.log("District");
+var getDistrict = function () {
+    params = getOptSelectedID("district", "districtData");
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState === xhttp.DONE) {
+            if (xhttp.response === "OK")
+                // Append result to datalist/select
+                console.log(xhttp.response);
+        }
+    };
+    xhttp.open("DISTRICT", "server.php?district=" + params);
+    xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhttp.send();
 };
-const getResident = () => {
-    console.log("Resident");
+var getResident = function () {
+    params = getOptSelectedID("resident", "residentData");
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState === xhttp.DONE) {
+            if (xhttp.response === "OK")
+                // Append result to datalist/select
+                console.log(xhttp.response);
+        }
+    };
+    xhttp.open("RESIDENT", "server.php?resident=" + params);
+    xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhttp.send();
 };
--- AJAX/index.ts
+++ AJAX/index.ts
@@ -1,11 +1,11 @@
 const xhttp = new XMLHttpRequest()
 let params
 
-const getOptSelectedID = (inputID : string, datalistID : string) => {
+const getOptSelectedID = (inputID: string, datalistID: string) => {
     const input = document.getElementById(inputID) as HTMLInputElement
     const elements = document.getElementById(datalistID) as HTMLDataListElement
     for (let index = 0; index < elements.options.length; index++) {
-        if(elements.options[index].value === input.value){
+        if (elements.options[index].value === input.value) {
             return elements.options[index].getAttribute("id")
         }
     }
@@ -15,6 +15,37 @@ const getOptSelectedID = (inputID : string, datalistID : string) => {
 const getCity = () => {
     params = getOptSelectedID("province", "provinceData");
     xhttp.onreadystatechange = () => {
+        if (xhttp.readyState === xhttp.DONE) {
+            if (xhttp.response === "OK")
+                // Append result to datalist/select
+                console.log(xhttp.response)
+        }
+
+    }
+    xhttp.open("CITY", `server.php?city=${params}`)
+    xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8')
+    xhttp.send()
+}
+
+
+const getDistrict = () => {
+    params = getOptSelectedID("district", "districtData");
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState === xhttp.DONE) {
+            if (xhttp.response === "OK")
+                // Append result to datalist/select
+                console.log(xhttp.response)
+        }
+
+    }
+    xhttp.open("DISTRICT", `server.php?district=${params}`)
+    xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8')
+    xhttp.send()
+}
+
+const getResident = () => {
+    params = getOptSelectedID("resident", "residentData");
+    xhttp.onreadystatechange = () => {
         if(xhttp.readyState === xhttp.DONE){
             if(xhttp.response === "OK")
             // Append result to datalist/select
@@ -22,16 +53,7 @@ const getCity = () => {
         }
 
     }
-    xhttp.open("CITY", "./server.php")
+    xhttp.open("RESIDENT", `server.php?resident=${params}`)
     xhttp.setRequestHeader('Content-type','application/json; charset=utf-8')
     xhttp.send()
-}
-
-
-const getDistrict = () => {
-    console.log("District")
-}
-
-const getResident = () => {
-    console.log("Resident")
 }
