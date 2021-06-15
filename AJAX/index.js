7b0eda1753aa815acd4bf19d9f73ec374ee1e546
StevenBudi <57691998+StevenBudi@users.noreply.github.com>
2021-06-15 16:32:46 +0700

    ajax

--- AJAX/index.js
+++ AJAX/index.js
@@ -2,6 +2,17 @@
 const xhttp = new XMLHttpRequest();
 const getCity = () => {
     console.log("City");
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState === xhttp.DONE) {
+            if (xhttp.response === "OK")
+                // Append result to datalist/select
+                console.log(xhttp.response);
+        }
+    };
+    const param = ""; // ID of selected value / selected value
+    xhttp.open("CITY", "./server.php");
+    xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhttp.send();
 };
 const getDistrict = () => {
     console.log("District");
